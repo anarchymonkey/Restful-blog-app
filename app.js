b7cec2e3f1f8f56bb403907d830e2606084a439e
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-26 12:27:21 +0530

    added mongoose and body-parser

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+app.set("view engine","ejs");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mongoose = require("mongoose");
+app.use(express.static("public"));
+/* ******************************************************************** */
+/*                      HOME PAGE                                       */
+/* ******************************************************************** */
+app.get("/",function(req,res){
+  console.log("ACCESSED THE INDEX PAGE");
+  res.render("index");
+});
+/* ******************************************************************** */
+/*                       BLOGS SITE                                     */
+/* ******************************************************************** */
+
+app.get("/blogs",function(req,res){
+  console.log("ACCESSED THE BLOGS PAGE WHERE VIEWERS WILL VIEW THE OBJECTS");
+  res.render("blogs");
+});
+
+/* ******************************************************************** */
+/*                       BLOGS CREATE                                   */
+/* ******************************************************************** */
+
+app.get("/blogs/create",function(req,res){
+  console.log("ACCESSED THE BLOGS/CREATE PAGE WHERE THE VIEWERS WILL TYPE THE BLOG");
+  res.render("createBlog");
+});
+
+/* ******************************************************************** */
+/*                       BLOGS UPDATE                                   */
+/* ******************************************************************** */
+app.get("/blogs/update",function(req,res){
+  console.log("ACCESSED THE BLOGS/UPDATE PAGE WHERE THE VIEWERS WILL UPDATE THE BLOGS");
+  res.render("updateBlog");
+});
+
+
+app.listen(3002,function(){
+  console.log("LISTENING ON PORT 3002");
+});
